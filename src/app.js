4b88f75aafe09e3e605adefe1d35721c5e1bd47c
Malcolm Kiano <malcolmkiano@gmail.com>
2020-04-27 12:49:00 -0400

    implemented user registration

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const { NODE_ENV } = require('./config');
 const thingsRouter = require('./things/things-router');
 const reviewsRouter = require('./reviews/reviews-router');
 const authRouter = require('./auth/auth-router');
+const usersRouter = require('./users/users-router');
 
 const app = express();
 
@@ -22,6 +23,7 @@ app.use(helmet());
 app.use('/api/things', thingsRouter);
 app.use('/api/reviews', reviewsRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/users', usersRouter);
 
 // eslint-disable-next-line no-unused-vars
 app.use(function errorHandler(error, req, res, next) {
